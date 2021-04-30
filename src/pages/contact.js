a13988105bf4de6cb62dee8d27e80062129799ec
Aaron Gertler <aaron.gertler@gmail.com>
2021-04-30 17:25:44 -0500

    linked pages together

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from "../components/header"
 
 export default function Contact() {
     return (
-        <div style={{ color: `teal` }}>
+        <div>
             <Link to="/">Home</Link>
-            <Header headerText="Contact" />
-            <p>Send us a message!</p>
+            <h1>I'd love to talk! Email me at the address below</h1>
+            <p>
+                <a href="mailto:me@example.com">me@example.com</a>
+            </p>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from "../components/header"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+    <div style={{ color: `purple`, margin: `3rem auto`, maxWidth: 600 }}>
+      <Link to="/about/">About</Link>
+      <br />
       <Link to="/contact/">Contact</Link>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building websites.
+      </p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
